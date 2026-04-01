acd52917d70ba3ebbb1d16aaab0c2272e5e018cf
AkhiBilal2005 <bilalahmed.cs24@bmsce.ac.in>
2026-04-01 10:43:07 +0530

    Update prims.c

--- prims.c
+++ prims.c
@@ -1,36 +1,66 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
+
 #define INF 9999999
-#define V 5
+
 
 int main()
 {
+    int V;
+    printf("Enter no. vertices: ");
+    scanf("%d", &V);
     int G[V][V];
-    int row, col;
-    printf("Enter the adjencancy matrix: \n");
-    for(row = 0 ; row < V ; row++){
-        for(col = 0 ; col < V ; col++){
-            scanf("%d", &G[row][col]);
+    srand(time(0));
+
+
+    for(int i = 0; i < V; i++){
+        for(int j = 0; j < V; j++){
+            if(i == j){
+                G[i][j] = 0;
+            } else if (j < i){
+                G[i][j] = G[j][i];
+            } else {
+                int r = rand() % 20 + 1;
+
+                if(rand() % 2)
+                    G[i][j] = r;
+                else
+                    G[i][j] = 0;
+            }
+        }
+    }
+
+
+    printf("Generated Adjacency Matrix:\n");
+    for(int i = 0; i < V; i++){
+        for(int j = 0; j < V; j++){
+            printf("%3d ", G[i][j]);
         }
         printf("\n");
     }
-    int no_e;
-    int sel[V];
+
+    int no_e = 0;
+    bool sel[V];
     memset(sel, false, sizeof(sel));
-    no_e = 0;
+
     sel[0] = true;
+
     int x, y;
-    printf("edge : Weight\n");
-    while(no_e <V-1){
+    printf("\nEdge : Weight\n");
+    clock_t start = clock();
+    while(no_e < V - 1){
         int min = INF;
         x = 0;
         y = 0;
-        for(int i = 0; i<V ; i++){
+
+        for(int i = 0; i < V; i++){
             if(sel[i]){
-                for (int j = 0; j < V ; j++){
+                for(int j = 0; j < V; j++){
                     if(!sel[j] && G[i][j]){
-                        if (min > G[i][j]){
+                        if(min > G[i][j]){
                             min = G[i][j];
                             x = i;
                             y = j;
@@ -39,9 +69,14 @@ int main()
                 }
             }
         }
-        printf("%d - %d : %d\n",x,y,G[x][y]);
+
+        printf("%d - %d : %d\n", x, y, G[x][y]);
         sel[y] = true;
         no_e++;
     }
+    clock_t end = clock();
+    double time_taken = (double)(end - start) / CLOCKS_PER_SEC;
+    printf("\nExecution time: %f seconds\n", time_taken);
+
     return 0;
 }
